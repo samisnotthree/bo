8fe4ade11fc800281d84907b61c2cb3641f1de7f
unknown <samisnotthree@gmail.com>
2022-01-21 19:50:19 +0900

    commit he project

---
+++ he/src/main/java/com/sint/he/HeApplication.java
@@ -0,0 +1,13 @@
+package com.sint.he;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeApplication.class, args);
+	}
+
+}
