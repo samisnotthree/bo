eb2a727b87658f652c33e2e49bcb78241eb49fb9
HyungEun Kwon <85722378+samisnotthree@users.noreply.github.com>
2022-02-27 01:41:50 +0900

    Update MemberRepository.java

--- he/src/main/java/com/sint/he/jpaTest/repository/MemberRepository.java
+++ he/src/main/java/com/sint/he/jpaTest/repository/MemberRepository.java
@@ -12,6 +12,8 @@ public interface MemberRepository extends JpaRepository<MemberVo, Long> {
 	//비워있어도 잘 작동함. 
 	// long 이 아니라 Long으로 작성. ex) int => Integer 같이 primitive형식 사용못함 
 	
+	public List<MemberVo> findByMbrNo(Long mbrNo);
+	
 	// findBy뒤에 컬럼명을 붙여주면 이를 이용한 검색이 가능하다 
 	public List<MemberVo> findById(String id); 
 	
